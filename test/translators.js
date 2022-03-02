bceda0f2bede6086d6f708771ac6c9c6f2974f16
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-03-02 11:49:54 +0100

    Adding dysfunctional test

--- test/translators.js
+++ test/translators.js
@@ -17,6 +17,12 @@ describe("Translators", function () {
         it("should work for simple input", function () {
             assert.strictEqual(toAllsprak("Hejsan hoppsan!"), "Hallejsan halloppsan!")
         })
+        it("should work for more simple input", function () {
+            assert.strictEqual(toAllsprak("Hejsan hoppsan!"), "Hallejsan halloppsan!")
+        })
+        it("should not work for simple input", function () {
+            assert.strictEqual(toRobber("Hejsan hoppsan!"), "Hohejojsosanon hohopoppopsosanon!")
+        })
     })
     describe("#toNormal", function () {
         it("should work for empty string", function () {
